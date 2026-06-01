1526a233b29e4a30c4b725d72cf9382a59115c82
IPFizzy <fizzy@keonbushman.com>
2026-06-01 13:55:44 -0700

    Add challenge features for CountToOne recursion

--- CountToOneRecursion/Program.cs
+++ CountToOneRecursion/Program.cs
@@ -12,30 +12,32 @@
 //---------------------------------------------------------------
 
 // Declare and initialize
-int choice = 0, result = 0;
+int choice = 0, result = 0, counter = 0;
 string input = "";
 
 // Prompt the user for a number
-Console.Write("Enter a positive number: ");
+Console.Write("Enter a number: ");
 
 // Get the users input
 input = Console.ReadLine();
 
 // See if the user entered valid input
-while (!int.TryParse(input, out choice) || choice <= 0)
+while (!int.TryParse(input, out choice))
 {
     Console.WriteLine("Invalid number");
 
     // Re-Prompt the user for a number
-    Console.Write("Enter a positive number: ");
+    Console.Write("Enter a number: ");
 
     // Get the users input
     input = Console.ReadLine();
 }
 
 // Call the CountToOne function
-result = Utility.CountToOne(choice);
+result = Utility.CountToOne(choice, ref counter);
+
 Console.WriteLine($"The end number is {result}");
+Console.WriteLine($"The number of recursive calls is {counter}");
 
 //---------------------------------------------------------------
 // End of the Main Method
@@ -47,11 +49,31 @@ static class Utility
     /// Count to one using recursion
     /// </summary>
     /// <param name="num"></param>
+    /// <param name="counter"></param>
     /// <returns></returns>
-    internal static int CountToOne(int num)
+    internal static int CountToOne(int num, ref int counter)
     {
         // Print out the current number
         Console.WriteLine($"The current number is {num}");
+
+        // Count the current recursive call
+        counter++;
+
+        // Check if the number is 0
+        if (num == 0)
+        {
+            Console.WriteLine("The number is 0. Change the number to 1");
+
+            return 1;
+        }
+
+        // Check if the number is negative
+        if (num < 0)
+        {
+            Console.WriteLine("The number is negative. Change the number to positive");
+
+            return CountToOne(Math.Abs(num), ref counter);
+        }
 
         // Check if the number is 1
         if (num == 1)
@@ -60,20 +82,41 @@ static class Utility
         }
         else
         {
-            // Check if the number is even
-            if ((num % 2) == 0)
+            // Check if the number is divisible by 4
+            if ((num % 4) == 0)
+            {
+                Console.WriteLine("The number is divisible by 4. Divide by 4");
+
+                // Divide the number by 4 and call the method
+                return CountToOne(num / 4, ref counter);
+            }
+            else if ((num % 3) == 0)
+            {
+                Console.WriteLine("The number is divisible by 3. Divide by 3");
+
+                // Divide the number by 3 and call the method
+                return CountToOne(num / 3, ref counter);
+            }
+            else if (((num % 5) == 0) && (num < 50) && (num > 5) && ((num % 2) != 0))
+            {
+                Console.WriteLine("The number is divisible by 5. Multiply by 2");
+
+                // Multiply the number by 2 and call the method
+                return CountToOne(num * 2, ref counter);
+            }
+            else if ((num % 2) == 0)
             {
                 Console.WriteLine("The number is even. Divide by 2");
 
                 // Divide the number by 2 and call the method
-                return CountToOne(num / 2);
+                return CountToOne(num / 2, ref counter);
             }
             else
             {
-                Console.WriteLine("The number is odd. Add 1");
+                Console.WriteLine("The number is odd. Subtract 1");
 
-                // Add 1 and call the method
-                return CountToOne(num + 1);
+                // Subtract 1 and call the method
+                return CountToOne(num - 1, ref counter);
             }
         }
     }
